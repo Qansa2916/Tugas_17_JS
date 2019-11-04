833fa78e103cdba1c22bb431956963a876ce1fbb
Qansa Alghifari <46017824+Qansa2916@users.noreply.github.com>
2019-11-04 10:32:42 +0700

    Tugas_17_JS

---
+++ Tugas-17.js
@@ -0,0 +1,16 @@
+function lowup() {
+    var x = 'Saya beLajar bahaSa peMrograman deNgan khUsuk.';
+    var y = 'Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual.';
+    console.log(x.toLowerCase());
+    console.log(x.toUpperCase());
+    console.log(y.toLowerCase());
+    console.log(y.toUpperCase());
+
+}
+lowup();
+
+function kodeascii() {
+    var x = 'Lorem Ipsum';
+    console.log(x.charCodeAt(0) + '-' + x.charCodeAt(1) + '-' + x.charCodeAt(2) + '-' + x.charCodeAt(3) + '-' + x.charCodeAt(4) + '-' + x.charCodeAt(5));
+}
+kodeascii();
